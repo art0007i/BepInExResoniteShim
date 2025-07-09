be0fdb23706e13f85682f70e2a8849430dfc75c5
art0007i <art0007i@gmail.com>
2025-07-09 18:07:13 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,140 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.NET.Common;
+using FrooxEngine;
+using HarmonyLib;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace BepInEx_RML_Loader;
+
+
+[BepInPlugin(GUID, Name, Version)]
+public class BepInEx_RML_Loader : BasePlugin
+{
+    public const string GUID = "ea80b2e7-e0ca-4bec-bb89-38e129e25cb1";
+    public const string Name = "me.art0007i.bepinex_rml_loader";
+    public const string Version = "0.1.0";
+
+    static ManualLogSource Logger = null!;
+    public override void Load()
+    {
+        Logger = Log;
+        var harmony = new Harmony("me.art0007i.bepinex_rml_loader");
+        harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(GraphicalClient.GraphicalClientRunner), nameof(GraphicalClient.GraphicalClientRunner.AssemblyDirectory), MethodType.Getter)]
+    public class AppPathFixer
+    {
+        public static bool Prefix(ref string __result)
+        {
+            __result = Paths.GameRootPath;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Engine), "InitializeFrooxEngine")]
+    public class RmlInit
+    {
+        public static void Postfix(ref Task __result)
+        {
+            var res = __result;
+            var newTask = Task.Run(async () =>
+            {
+                await res;
+
+                var asm = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(x => x.GetName().Name == "ResoniteModLoader");
+                if(asm == null)
+                {
+                    Logger.LogWarning("Failed to initialize ResoniteModLoader: ResoniteModLoader.dll is not loaded!");
+                    return;
+                }
+                var hook = asm.GetType("ResoniteModLoader.ExecutionHook");
+                AccessTools.Field(hook, "__connectorType").SetValue(null, typeof(bool));
+            });
+
+            __result = newTask;
+        }
+    }
+
+    [HarmonyPatch]
+    public class LocationFixer
+    {
+        public static MethodBase TargetMethod()
+        {
+            return AccessTools.FirstConstructor(typeof(AssemblyTypeRegistry), (x)=>x.GetParameters().Length > 3);
+        }
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+        {
+            int killCount = 0;
+            var cachePath = AccessTools.PropertyGetter(typeof(GlobalTypeRegistry), nameof(GlobalTypeRegistry.MetadataCachePath));
+            var asmLoc = AccessTools.PropertyGetter(typeof(Assembly), nameof(Assembly.Location));
+
+            foreach (var code in codes)
+            {
+                if (killCount > 0)
+                {
+                    killCount--;
+                    continue;
+                }
+                if (code.Calls(asmLoc))
+                {
+                    Logger.LogDebug("Patched AsmLoc");
+
+                    yield return new(OpCodes.Call, AccessTools.Method(typeof(LocationFixer), nameof(ProcessCacheTime)));
+                    killCount = 2; // skip next code
+                }
+                else
+                {
+                    yield return code;
+                }
+                if(code.Calls(cachePath))
+                {
+                    Logger.LogDebug("Patched CachePath");
+
+                    yield return new(OpCodes.Ldarg_1); // assembly
+                    yield return new(OpCodes.Call, AccessTools.Method(typeof(LocationFixer), nameof(ProcessCachePath)));
+                }
+            }
+        }
+
+        public static string? ProcessCachePath(string cachePath, Assembly asm)
+        {
+            if (string.IsNullOrWhiteSpace(asm.Location)) return null;
+            return cachePath;
+        }
+
+        public static DateTime ProcessCacheTime(Assembly asm)
+        {
+            if (string.IsNullOrWhiteSpace(asm.Location)) return DateTime.UtcNow;
+            return new FileInfo(asm.Location).LastWriteTimeUtc;
+        }
+    }
+
+    [HarmonyPatch(typeof(EngineInitializer), nameof(EngineInitializer.InitializeFrooxEngine), MethodType.Async)]
+    public class AssemblyLoadFixer
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+        {
+            var loadFrom = AccessTools.Method(typeof(Assembly), nameof(Assembly.LoadFrom), [typeof(string)]);
+            foreach (var code in codes)
+            {
+                if(code.Calls(loadFrom))
+                {
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(AssemblyLoadFixer), nameof(LoadFrom)));
+                }
+                else
+                {
+                    yield return code;
+                }
+            }
+        }
+
+        public static Assembly? LoadFrom(string path)
+        {
+            Logger.LogInfo("Bypassing LoadFrom: " + path);
+            return null;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# BepInEx_RML_Loader
+
+Plugin for my net9 bepinex fork for resonite which applies some patches to allow the game to run, and attempts to load ResoniteModLoader.dll
